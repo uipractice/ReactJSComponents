77e50f3b9408cc1cd9ab4b2aebb32366f10b517c
parvezrashid <pmohammed@evoketechnologies.com>
2023-03-10 20:56:18 +0530

    Update material card

--- src/components/utility/Cards/MatCards.js
+++ src/components/utility/Cards/MatCards.js
@@ -156,68 +156,71 @@ const bull = (
         <MediaControlCard />
       </div>
     </div>
-  </div>
+  </div>`}</code><code className="language-javascript">
+          {`
 
-      function MediaCard() {
-          return (
-            <Card sx={{ maxWidth: 345 }}>
-              <CardMedia
-                sx={{ height: 140 }}
-                image={reptile}
-                title="green iguana"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  Lizard
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Lizards are a widespread group of squamate reptiles, with over 6,000
-                  species, ranging across all continents except Antarctica
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small">Share</Button>
-                <Button size="small">Learn More</Button>
-              </CardActions>
-            </Card>
-          );
-        }
+<====================MatCardWithImage.js======================>
+
+  function MediaCard() {
+      return (`}</code><code className="language-markup">{`
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            sx={{ height: 140 }}
+            image={reptile}
+            title="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Lizard
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Lizards are a widespread group of squamate reptiles, with over 6,000
+              species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>`}</code><code className="language-javascript">{`
+      );
+    }
   );
 }
 
     function MediaControlCard() {
       const theme = useTheme();
     
-      return (
-        <Card sx={{ display: 'flex' }}>
-          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-            <CardContent sx={{ flex: '1 0 auto' }}>
-              <Typography component="div" variant="h5">
-                Live From Space
-              </Typography>
-              <Typography variant="subtitle1" color="text.secondary" component="div">
-                Mac Miller
-              </Typography>
-            </CardContent>
-            <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
-              <IconButton aria-label="previous">
-                {theme.direction === 'rtl' ? <SkipNextIcon /> : <SkipPreviousIcon />}
-              </IconButton>
-              <IconButton aria-label="play/pause">
-                <PlayArrowIcon sx={{ height: 38, width: 38 }} />
-              </IconButton>
-              <IconButton aria-label="next">
-                {theme.direction === 'rtl' ? <SkipPreviousIcon /> : <SkipNextIcon />}
-              </IconButton>
-            </Box>
+      return (`}</code><code className="language-markup">{`
+      <Card sx={{ display: 'flex' }}>
+        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+          <CardContent sx={{ flex: '1 0 auto' }}>
+            <Typography component="div" variant="h5">
+              Live From Space
+            </Typography>
+            <Typography variant="subtitle1" color="text.secondary" component="div">
+              Mac Miller
+            </Typography>
+          </CardContent>
+          <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+            <IconButton aria-label="previous">
+              {theme.direction === 'rtl' ? <SkipNextIcon /> : <SkipPreviousIcon />}
+            </IconButton>
+            <IconButton aria-label="play/pause">
+              <PlayArrowIcon sx={{ height: 38, width: 38 }} />
+            </IconButton>
+            <IconButton aria-label="next">
+              {theme.direction === 'rtl' ? <SkipPreviousIcon /> : <SkipNextIcon />}
+            </IconButton>
           </Box>
-          <CardMedia
-            component="img"
-            sx={{ width: 151 }}
-            image={liveFromSpace}
-            alt="Live from space album cover"
-          />
-        </Card>
+        </Box>
+        <CardMedia
+          component="img"
+          sx={{ width: 151 }}
+          image={liveFromSpace}
+          alt="Live from space album cover"
+        />
+      </Card>`}</code><code className="language-javascript">{`
       );
     }
 
@@ -225,7 +228,7 @@ const bull = (
 
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
-  return <IconButton {...other} />;
+  return `}</code><code className="language-markup">{`<IconButton {...other} />`}</code><code className="language-javascript">{`;
 })(({ theme, expand }) => ({
   transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
   marginLeft: 'auto',
@@ -241,87 +244,86 @@ function RecipeReviewCard() {
     setExpanded(!expanded);
   };
 
-  return (
+  return (`}</code><code className="language-markup">{`
     <Card sx={{ maxWidth: 345 }}>
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
-      />
-      <CardMedia
-        component="img"
-        height="194"
-        image={pella}
-        alt="Paella dish"
-      />
+    <CardHeader
+      avatar={
+        <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+          R
+        </Avatar>
+      }
+      action={
+        <IconButton aria-label="settings">
+          <MoreVertIcon />
+        </IconButton>
+      }
+      title="Shrimp and Chorizo Paella"
+      subheader="September 14, 2016"
+    />
+    <CardMedia
+      component="img"
+      height="194"
+      image={pella}
+      alt="Paella dish"
+    />
+    <CardContent>
+      <Typography variant="body2" color="text.secondary">
+        This impressive paella is a perfect party dish and a fun meal to cook
+        together with your guests. Add 1 cup of frozen peas along with the mussels,
+        if you like.
+      </Typography>
+    </CardContent>
+    <CardActions disableSpacing>
+      <IconButton aria-label="add to favorites">
+        <FavoriteIcon />
+      </IconButton>
+      <IconButton aria-label="share">
+        <ShareIcon />
+      </IconButton>
+      <ExpandMore
+        expand={expanded}
+        onClick={handleExpandClick}
+        aria-expanded={expanded}
+        aria-label="show more"
+      >
+        <ExpandMoreIcon />
+      </ExpandMore>
+    </CardActions>
+    <Collapse in={expanded} timeout="auto" unmountOnExit>
       <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the mussels,
-          if you like.
+        <Typography paragraph>Method:</Typography>
+        <Typography paragraph>
+          Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
+          aside for 10 minutes.
+        </Typography>
+        <Typography paragraph>
+          Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
+          medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
+          occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
+          large plate and set aside, leaving chicken and chorizo in the pan. Add
+          pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
+          stirring often until thickened and fragrant, about 10 minutes. Add
+          saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+        </Typography>
+        <Typography paragraph>
+          Add rice and stir very gently to distribute. Top with artichokes and
+          peppers, and cook without stirring, until most of the liquid is absorbed,
+          15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
+          mussels, tucking them down into the rice, and cook again without
+          stirring, until mussels have opened and rice is just tender, 5 to 7
+          minutes more. (Discard any mussels that don&apos;t open.)
+        </Typography>
+        <Typography>
+          Set aside off of the heat to let rest for 10 minutes, and then serve.
         </Typography>
       </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-        <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </ExpandMore>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-            aside for 10 minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-            medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-            occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-            large plate and set aside, leaving chicken and chorizo in the pan. Add
-            pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-            stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and
-            peppers, and cook without stirring, until most of the liquid is absorbed,
-            15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-            mussels, tucking them down into the rice, and cook again without
-            stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don&apos;t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
-          </Typography>
-        </CardContent>
-      </Collapse>
-    </Card>
+    </Collapse>
+    </Card>`}</code><code className="language-javascript">{`
   );
 }
 
 export { RecipeReviewCard, MediaCard, MediaControlCard };
-
-        `}</code>
+`}</code>
       </pre>
     </div>
   );
