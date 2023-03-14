03251f0c64d2e861b83e8313947d0e6c2222b9ee
Parvez Rashid Mohammed <pmohammed@evoketechnologies.com>
2023-03-14 19:43:02 +0530

    Update material basic table (code tab)

--- src/components/tables/Table/MatBasicTable.js
+++ src/components/tables/Table/MatBasicTable.js
@@ -206,63 +206,64 @@ export const MatBasicTableCode = () => {
     <h4>React Material UI code</h4>
     <pre style={{ marginRight: '20px', background: '#fff' }}>
       <code className='language-javascript'>{`< ------- MatBasicTable.js ------- >
-    import * as React from 'react';
-    import { styled } from '@mui/material/styles';
-    import Table from '@mui/material/Table';
-    import TableBody from '@mui/material/TableBody';
-    import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-    import TableContainer from '@mui/material/TableContainer';
-    import TableHead from '@mui/material/TableHead';
-    import TableRow from '@mui/material/TableRow';
-    import Paper from '@mui/material/Paper';
+      
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
-[\`&.\${tableCellClasses.head}\`]: {
-backgroundColor: theme.palette.common.white,
-color: theme.palette.common.black,
-},
-[\`&.\${tableCellClasses.body}\`]: {
-fontSize: 14,
-},
+    [\`&.\${tableCellClasses.head}\`]: {
+    backgroundColor: theme.palette.common.white,
+    color: theme.palette.common.black,
+  },
+    [\`&.\${tableCellClasses.body}\`]: {
+    fontSize: 14,
+  },
 }));
+
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
-'&:nth-of-type(odd)': {
-backgroundColor: theme.palette.action.hover,
-},
-// hide last border
-'&:last-child td, &:last-child th': {
-border: 0,
-},
+  '&:nth-of-type(odd)': {
+  backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+  border: 0,
+  },
 }));
+
 // Styles for Table 3
 const DarkStyledTableCell = styled(TableCell)(({ theme }) => ({
-[\`&.\${tableCellClasses.head}\`]: {
-backgroundColor: theme.palette.common.black,
-color: theme.palette.common.white,
-},
-[\`&.\${tableCellClasses.body}\`]: {
-fontSize: 14,
-backgroundColor:theme.palette.common.black,
-color: theme.palette.common.white,
-},
+  [\`&.\${tableCellClasses.head}\`]: {
+  backgroundColor: theme.palette.common.black,
+  color: theme.palette.common.white,
+  },
+  [\`&.\${tableCellClasses.body}\`]: {
+  fontSize: 14,
+  backgroundColor:theme.palette.common.black,
+  color: theme.palette.common.white,
+  },
 }));
 // constants
 function createData(name, calories, fat, carbs, protein) {
-return { name, calories, fat, carbs, protein };
+  return { name, calories, fat, carbs, protein };
 }
 const rows = [
-createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-createData('Eclair', 262, 16.0, 24, 6.0),
-createData('Cupcake', 305, 3.7, 67, 4.3),
-createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
 ];
 
 export const MatBasicTable = () => {
-        return (`}</code>
-
-      <code className='language-markup'>
-        {`<TableContainer component={Paper}>
+  return (`}</code><code className='language-markup'>{`
+    <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
