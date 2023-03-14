72b854ea70304fe2f8977bba82ea52c3e5977f5d
Parvez Rashid Mohammed <pmohammed@evoketechnologies.com>
2023-03-14 14:12:44 +0530

    Update react-bootstrap badge

--- src/components/basic/Badge/RbBadge.js
+++ src/components/basic/Badge/RbBadge.js
@@ -98,78 +98,82 @@ import Badge from "react-bootstrap/Badge";
 import Button from "react-bootstrap/Button";
 
 export function RdBadge() {
-return (
-    <div className='demo-wrapper'>
-	<div>
-		<b>Notification Indication Badge with multiple variants</b>
-    <br></br>
-		<Button variant="secondary">Primary<Badge bg="primary">10</Badge></Button>{" "}
-		<Button variant="secondary">Secondary<Badge bg="secondary">10</Badge></Button>{" "}
-		<Button variant="secondary">Success<Badge bg="success">10</Badge></Button>{" "}
-		<Button variant="secondary">Danger<Badge bg="danger">10</Badge></Button>{" "}
-		<Button variant="secondary">Warning<Badge bg="warning">10</Badge></Button>{" "}
-		<Button variant="secondary">Light<Badge bg="light" text="dark">10</Badge></Button>{" "}
-	</div>
-	<br />
+return (`}</code><code className="language-markup">{`
+  <div className='demo-wrapper'>
+    <div>
+      <b>Notification Indication Badge with multiple variants</b>
+      <br></br>
+      <Button variant="secondary">Primary<Badge bg="primary">10</Badge></Button>{" "}
+      <Button variant="secondary">Secondary<Badge bg="secondary">10</Badge></Button>{" "}
+      <Button variant="secondary">Success<Badge bg="success">10</Badge></Button>{" "}
+      <Button variant="secondary">Danger<Badge bg="danger">10</Badge></Button>{" "}
+      <Button variant="secondary">Warning<Badge bg="warning">10</Badge></Button>{" "}
+      <Button variant="secondary">Light<Badge bg="light" text="dark">10</Badge></Button>{" "}
+    </div>
+    <br />
+    <div>
+      <b>Pill Type Badge with multiple variants<b>
+      <br></br>
+      <Badge pill bg="primary">
+          Primary
+        </Badge>{' '}
+        <Badge pill bg="secondary">
+          Secondary
+        </Badge>{' '}
+        <Badge pill bg="success">
+          Success
+        </Badge>{' '}
+        <Badge pill bg="danger">
+          Danger
+        </Badge>{' '}
+        <Badge pill bg="warning" text="dark">
+          Warning
+        </Badge>{' '}
+        <Badge pill bg="info">
+          Info
+        </Badge>{' '}
+        <Badge pill bg="light" text="dark">
+          Light
+        </Badge>{' '}
+        <Badge pill bg="dark">
+          Dark
+        </Badge>
+    </div>
+    <br />
+    <div>
+      <b>Contextual Badge with multiple variants</b>
+      <br></br>
+      <Badge bg="primary">Primary variant</Badge>{" "}
+      <Badge bg="secondary">Secondary variant</Badge>{" "}
+      <Badge bg="success">Success variant</Badge>{" "}
+      <Badge bg="danger">Danger variant</Badge>{" "}
+      <Badge bg="warning">Warning variant</Badge>{" "}
+      <Badge bg="info">Info variant</Badge>{" "}
+      <Badge bg="light" text="dark">Light variant</Badge>{" "}
+      <Badge bg="dark">Dark variant </Badge>
+    </div>
+    <br />
+    <div>
+      <b>Use of badges in normal text</b>
+      <br></br>
+      <h5>Heading<Badge bg="primary">primary badge variant</Badge></h5>
+      <h5>Heading<Badge bg="secondary">secondary badge variant</Badge></h5>
+      <h5>Heading<Badge bg="success">success badge variant</Badge></h5>
+      <h5>Heading<Badge bg="danger">danger badge variant</Badge></h5>
+      <h5>Heading<Badge bg="warning">warning badge variant</Badge></h5>
+      <h5>Heading<Badge bg="info">info badge variant</Badge></h5>
+      <h5>Heading<Badge bg="dark">dark badge variant</Badge></h5>
+    </div>
+  </div>
+  `}
+        </code><code className="language-javascript">
+          {`
+  );
+}
 
-	<div>
-		<b>Pill Type Badge with multiple variants<b>
-    <br></br>
-		<Badge pill bg="primary">
-        Primary
-      </Badge>{' '}
-      <Badge pill bg="secondary">
-        Secondary
-      </Badge>{' '}
-      <Badge pill bg="success">
-        Success
-      </Badge>{' '}
-      <Badge pill bg="danger">
-        Danger
-      </Badge>{' '}
-      <Badge pill bg="warning" text="dark">
-        Warning
-      </Badge>{' '}
-      <Badge pill bg="info">
-        Info
-      </Badge>{' '}
-      <Badge pill bg="light" text="dark">
-        Light
-      </Badge>{' '}
-      <Badge pill bg="dark">
-        Dark
-      </Badge>
-	</div>
-	<br />
-	<div>
-		<b>Contextual Badge with multiple variants</b>
-    <br></br>
-		<Badge bg="primary">Primary variant</Badge>{" "}
-		<Badge bg="secondary">Secondary variant</Badge>{" "}
-		<Badge bg="success">Success variant</Badge>{" "}
-		<Badge bg="danger">Danger variant</Badge>{" "}
-		<Badge bg="warning">Warning variant</Badge>{" "}
-		<Badge bg="info">Info variant</Badge>{" "}
-		<Badge bg="light" text="dark">Light variant</Badge>{" "}
-		<Badge bg="dark">Dark variant </Badge>
-	</div>
-	<br />
-	<div>
-		<b>Use of badges in normal text</b>
-    <br></br>
-		<h5>Heading  <Badge bg="primary">primary badge variant !!!</Badge></h5>
-		<h5>Heading  <Badge bg="secondary">secondary badge variant</Badge></h5>
-		<h5>Heading  <Badge bg="success">success badge variant</Badge></h5>
-		<h5>Heading  <Badge bg="danger">danger badge variant</Badge></h5>
-		<h5>Heading  <Badge bg="warning">warning badge variant</Badge></h5>
-		<h5>Heading  <Badge bg="info">info badge variant</Badge></h5>
-		<h5>Heading  <Badge bg="dark">dark badge variant</Badge></h5>
-	</div>
-	</div>
-);
-}          
 export default RdBadge;
-`}</code>
+  `}
+        </code>
       </pre>
     </div>
   )
