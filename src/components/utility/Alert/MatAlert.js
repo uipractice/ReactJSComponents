867af39f80c8e3a053f2d52ad6747bba6e28e78d
Parvez Rashid Mohammed <pmohammed@evoketechnologies.com>
2023-03-14 15:56:27 +0530

    Update material alert

--- src/components/utility/Alert/MatAlert.js
+++ src/components/utility/Alert/MatAlert.js
@@ -98,94 +98,86 @@ function MatAlertCode() {
       <h4>React Material UI code</h4>
       <pre style={{ marginRight: '20px', background: '#fff' }}>
         <code className='language-javascript'>{`
-import { makeStyles } from '@material-ui/core/styles';
 import Alert from '@mui/material/Alert';
 import AlertTitle from '@mui/material/AlertTitle';
 import IconButton from '@mui/material/IconButton';
 import Collapse from '@mui/material/Collapse';
 import Button from '@mui/material/Button';
 import CloseIcon from '@mui/icons-material/Close';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '90%',
-    '& > * + *': {
-      marginTop: theme.spacing(2),
-    },
-  },
-}));
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
 
 function MatAlert() {
   const [open, setOpen] = useState(true);
-  const classes = useStyles();
 
   return (`}</code>
         <code className='language-markup'>{`
     <div className='demo-wrapper'>
-    <div style={{marginBottom: '10px'}}><strong>Alert with Trigger Button</strong></div>
-    <div className={classes.root}>
-      <Collapse in={open}>
-          <Alert
-            action={
-              <IconButton
-                aria-label="close"
-                color="inherit"
-                size="small"
-                onClick={() => {
-                  setOpen(false);
-                }}
-              >
-                <CloseIcon fontSize="inherit" />
-              </IconButton>
-            }
-            sx={{ mb: 2 }}
+      <div style={{marginBottom: '10px'}}><strong>Alert with Trigger Button</strong></div>
+      <Box sx={{ width: '90%' }}>
+        <Collapse in={open}>
+            <Alert
+              action={
+                <IconButton
+                  aria-label="close"
+                  color="inherit"
+                  size="small"
+                  onClick={() => {
+                    setOpen(false);
+                  }}
+                >
+                  <CloseIcon fontSize="inherit" />
+                </IconButton>
+              }
+              sx={{ mb: 2 }}
+            >
+              Close me!
+            </Alert>
+          </Collapse>
+          <Button
+            disabled={open}
+            variant="outlined"
+            onClick={() => {
+              setOpen(true);
+            }}
           >
-            Close me!
-          </Alert>
-        </Collapse>
-        <Button
-          disabled={open}
-          variant="outlined"
-          onClick={() => {
-            setOpen(true);
-          }}
-        >
-          Re-open
-        </Button>
+            Re-open
+          </Button>
+        </Box>
+      <div style={{marginTop: '10px'}}><strong>Simple Alerts with four severity levels </strong></div>
+      <Stack sx={{ width: '90%' }} spacing={2}>
+        <Alert severity="error">This is an error alert — check it out!</Alert>
+        <Alert severity="warning">This is a warning alert — check it out!</Alert>
+        <Alert severity="info">This is an info alert — check it out!</Alert>
+        <Alert severity="success">This is a success alert — check it out!</Alert>
+      </Stack>
+      <div style={{marginTop: '10px'}}><strong>Alert with <code>AlertTitle</code> component </strong></div>
+      <Stack sx={{ width: '90%' }} spacing={2}>
+        <Alert severity="error">
+          <AlertTitle>Error</AlertTitle>
+          This is an error alert — <strong>check it out!</strong>
+        </Alert>
+        <Alert severity="warning">
+          <AlertTitle>Warning</AlertTitle>
+          This is a warning alert — <strong>check it out!</strong>
+        </Alert>
+        <Alert severity="info">
+          <AlertTitle>Info</AlertTitle>
+          This is an info alert — <strong>check it out!</strong>
+        </Alert>
+        <Alert severity="success">
+          <AlertTitle>Success</AlertTitle>
+          This is a success alert — <strong>check it out!</strong>
+        </Alert>
+      </Stack>
     </div>
-    <div style={{marginTop: '10px'}}><strong>Simple Alerts with four severity levels </strong></div>
-    <div className={classes.root} style={{marginTop: '10px'}}>
-      <Alert severity="error">This is an error alert — check it out!</Alert>
-      <Alert severity="warning">This is a warning alert — check it out!</Alert>
-      <Alert severity="info">This is an info alert — check it out!</Alert>
-      <Alert severity="success">This is a success alert — check it out!</Alert>
-    </div>
-    <div style={{marginTop: '10px'}}><strong>Alert with <code>AlertTitle</code> component </strong></div>
-    <div className={classes.root} style={{marginTop: '10px'}}>
-      <Alert severity="error">
-        <AlertTitle>Error</AlertTitle>
-        This is an error alert — <strong>check it out!</strong>
-      </Alert>
-      <Alert severity="warning">
-        <AlertTitle>Warning</AlertTitle>
-        This is a warning alert — <strong>check it out!</strong>
-      </Alert>
-      <Alert severity="info">
-        <AlertTitle>Info</AlertTitle>
-        This is an info alert — <strong>check it out!</strong>
-      </Alert>
-      <Alert severity="success">
-        <AlertTitle>Success</AlertTitle>
-        This is a success alert — <strong>check it out!</strong>
-      </Alert>
-    </div>
-  </div>
   `}</code>
         <code className='language-javascript'>{`
-);
-}export default MatAlert;
-`}</code>
+  );
+}
 
+export default MatAlert;
+`}</code>
       </pre>
     </div>
   );
