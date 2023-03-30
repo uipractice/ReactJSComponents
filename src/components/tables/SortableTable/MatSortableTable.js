adc868a2efdc8d958ee371c5a188d557a8ff091c
Parvez Rashid Mohammed <pmohammed@evoketechnologies.com>
2023-03-30 15:33:07 +0530

    Update material sortable table

--- src/components/tables/SortableTable/MatSortableTable.js
+++ src/components/tables/SortableTable/MatSortableTable.js
@@ -1,67 +1,116 @@
-import React, { useEffect } from "react";
-import Spinner from "react-bootstrap/Spinner";
+import React, { useEffect, useMemo } from "react";
+import MaterialReactTable from 'material-react-table';
 import Prism from "prismjs";
 import "../../prism.css";
-import MaterialTable from "@material-table/core";
-import { ApiDataTableFetch } from "../../services/ApiDataTableFetch"
 
 export const MatSortableTable = () => {
-  const columns = [
-    { title: 'id', field: 'id' },
-    { title: 'Name', field: 'name' },
-    { title: 'Email', field: 'email'},
-    { title: 'Post Id', field: 'postId'}
-    ];
-  // useEffect(() => {
-  //   window.scrollTo(0, 0);
-  //   Prism.highlightAll();
-  //   if (!data.length)
-  //     fetch
-  //       .then((response) => response.json())
-  //       .then((result) => {
-  //         setData(result.data);
-  //       });
-  // });
-  const { data, error } = ApiDataTableFetch(        "https://jsonplaceholder.typicode.com/comments"
-  )
-if (error) {
-return error.message;
-}
+
+  const columns = useMemo(() => [
+    { header: "ID", accessorKey: "id" },
+    { header: "Name", accessorKey: "name" },
+    { header: "Username", accessorKey: "username" },
+    { header: "Email", accessorKey: "email" },
+  ],
+    [],
+  );
+
+  const data = useMemo(() => [
+    {
+      "id": 1,
+      "name": "Leanne Graham",
+      "username": "Bret",
+      "email": "Sincere@april.biz",
+    },
+    {
+      "id": 2,
+      "name": "Ervin Howell",
+      "username": "Antonette",
+      "email": "Shanna@melissa.tv",
+    },
+    {
+      "id": 3,
+      "name": "Clementine Bauch",
+      "username": "Samantha",
+      "email": "Nathan@yesenia.net",
+    },
+    {
+      "id": 4,
+      "name": "Patricia Lebsack",
+      "username": "Karianne",
+      "email": "Julianne.OConner@kory.org",
+    },
+    {
+      "id": 5,
+      "name": "Chelsey Dietrich",
+      "username": "Kamren",
+      "email": "Lucio_Hettinger@annie.ca",
+    },
+    {
+      "id": 6,
+      "name": "Mrs. Dennis Schulist",
+      "username": "Leopoldo_Corkery",
+      "email": "Karley_Dach@jasper.info",
+    },
+    {
+      "id": 7,
+      "name": "Kurtis Weissnat",
+      "username": "Elwyn.Skiles",
+      "email": "Telly.Hoeger@billy.biz",
+    },
+    {
+      "id": 8,
+      "name": "Nicholas Runolfsdottir V",
+      "username": "Maxime_Nienow",
+      "email": "Sherwood@rosamond.me",
+    },
+    {
+      "id": 9,
+      "name": "Glenna Reichert",
+      "username": "Delphine",
+      "email": "Chaim_McDermott@dana.io",
+    },
+    {
+      "id": 10,
+      "name": "Clementina DuBuque",
+      "username": "Moriah.Stanton",
+      "email": "Rey.Padberg@karina.biz",
+    }
+
+  ],
+    [],
+  );
 
   return (
     <div className="demo-wrapper">
       <div style={{ marginBottom: "10px" }}>
-      Sorting functionality allows you to sort the data of the tables according to any specific columns.
+        Sorting functionality allows you to sort the data of the tables according to any specific columns.
       </div>
       <div style={{ marginRight: "20px" }}>
-        {data ? (
-          <MaterialTable
-            title="Sortable Table"
-            columns={columns}
-            data={data}
-            options={{
-              sorting: true,
-              headerStyle: {
-                backgroundColor: "#01579b",
-                color: "#FFF",
-              },
-            }}
-          />
-        ) : (
-          <Spinner animation="border" variant="dark" />
-        )}
+        <MaterialReactTable
+          columns={columns}
+          data={data}
+          enableColumnActions={true}
+          enableColumnFilters={false}
+          enablePagination={true}
+          enableSorting={true}
+          enableBottomToolbar={true}
+          enableTopToolbar={false}
+          muiTableBodyRowProps={{ hover: false }}
+          muiTablePaperProps={{ sx: { borderRadius: '8px' } }}
+          muiTableHeadRowProps={{ sx: { backgroundColor: '#01579b' } }}
+          muiTableHeadCellProps={{ sx: { color: "#fff" } }}
+        />
       </div>
       <div className='compo-description'>
-                <h4>Description</h4>
-                <p>Sorting functionality allows you to sort the data of the tables according to any specific columns.</p>
-                <h5>Steps</h5>
-                <ul>
-                <li><strong>Step 1 :</strong> Install Dependency to your project root folder : <code>npm install @material-ui/core material-table --save-dev;</code><br/>
-                Add Font Icons Dependency to your root html file <code>{`<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>`}</code></li>
-                <li><strong>Step 2 :</strong> Import components, for Table: <code>import MaterialTable from '@material-table/core';</code></li>
-                <li><strong>Step 3 :</strong> Refer React Material UI code under the 'Code' tab, to understand the usage of component.</li>
-            </ul>
-            </div>
+        <h4>Description</h4>
+        <p>Sorting functionality allows you to sort the data of the tables according to any specific columns.</p>
+        <h5>Steps</h5>
+        <ul>
+          <li><strong>Step 1 :</strong> Install Dependency to your project root folder : <code>npm install material-react-table</code><br /></li>
+          <li><strong>Step 2 :</strong> Import components, for Table: <code>import MaterialReactTable from '@material-react-table';</code></li>
+          <li><strong>Step 3 :</strong> Refer React Material UI code under the 'Code' tab, to understand the usage of component.</li>
+        </ul>
+      </div>
     </div>
   );
 };
@@ -69,87 +118,117 @@ return error.message;
 export const MatSortableTableCode = () => {
   useEffect(() => Prism.highlightAll(), []);
   return (
-  <>
-  <div className='jsx-code-wrapper'>
-      <h4>React Material UI code</h4>
-      <pre style={{marginRight: '20px', background: '#fff'}}>
-              <code className='language-javascript'>{`<=======MatSortingTable.js=======>
-import React, { useState, useEffect } from "react";
-import Spinner from "react-bootstrap/Spinner";
-import MaterialTable from "@material-table/core";
-import { ApiDataTableFetch } from "../../services/ApiDataTableFetch"
+    <>
+      <div className='jsx-code-wrapper'>
+        <h4>React Material UI code</h4>
+        <pre style={{ marginRight: '20px', background: '#fff' }}>
+          <code className='language-javascript'>{`
+import React, { useEffect, useMemo } from "react";
+import MaterialReactTable from 'material-react-table';
 
 export const MatSortableTable = () => {
-  const columns = [
-    { title: 'id', field: 'id' },
-    { title: 'Name', field: 'name' },
-    { title: 'Email', field: 'email'},
-    { title: 'Post Id', field: 'postId'}
-    ];
-};
-const { data, error } = ApiDataTableFetch("https://jsonplaceholder.typicode.com/comments")
-if (error) {
-return error.message;
-}
+
+  const columns = useMemo(() => [
+    { header: "ID", accessorKey: "id" },
+    { header: "Name", accessorKey: "name" },
+    { header: "Username", accessorKey: "username" },
+    { header: "Email", accessorKey: "email" },
+  ],
+    [],
+  );
+
+  const data = useMemo(() => [
+    {
+      "id": 1,
+      "name": "Leanne Graham",
+      "username": "Bret",
+      "email": "Sincere@april.biz",
+    },
+    {
+      "id": 2,
+      "name": "Ervin Howell",
+      "username": "Antonette",
+      "email": "Shanna@melissa.tv",
+    },
+    {
+      "id": 3,
+      "name": "Clementine Bauch",
+      "username": "Samantha",
+      "email": "Nathan@yesenia.net",
+    },
+    {
+      "id": 4,
+      "name": "Patricia Lebsack",
+      "username": "Karianne",
+      "email": "Julianne.OConner@kory.org",
+    },
+    {
+      "id": 5,
+      "name": "Chelsey Dietrich",
+      "username": "Kamren",
+      "email": "Lucio_Hettinger@annie.ca",
+    },
+    {
+      "id": 6,
+      "name": "Mrs. Dennis Schulist",
+      "username": "Leopoldo_Corkery",
+      "email": "Karley_Dach@jasper.info",
+    },
+    {
+      "id": 7,
+      "name": "Kurtis Weissnat",
+      "username": "Elwyn.Skiles",
+      "email": "Telly.Hoeger@billy.biz",
+    },
+    {
+      "id": 8,
+      "name": "Nicholas Runolfsdottir V",
+      "username": "Maxime_Nienow",
+      "email": "Sherwood@rosamond.me",
+    },
+    {
+      "id": 9,
+      "name": "Glenna Reichert",
+      "username": "Delphine",
+      "email": "Chaim_McDermott@dana.io",
+    },
+    {
+      "id": 10,
+      "name": "Clementina DuBuque",
+      "username": "Moriah.Stanton",
+      "email": "Rey.Padberg@karina.biz",
+    }
+
+  ],
+    [],
+  );
 
   return (`}</code><code className='language-markup'>{`
-  <div style={{ marginBottom: "10px" }}>
-  Sorting functionality allows you to sort the data of the tables according to any specific columns.
+  <div className="demo-wrapper">
+      <div style={{ marginBottom: "10px" }}>
+        Sorting functionality allows you to sort the data of the tables according to any specific columns.
       </div>
       <div style={{ marginRight: "20px" }}>
-        {data ? (
-          <MaterialTable
-            title="Sortable Table"
-            columns={columns}
-            data={data}
-            options={{
-              sorting: true,
-              headerStyle: {
-                backgroundColor: "#01579b",
-                color: "#FFF",
-              },
-            }}
-          />
-        ) : (
-          <Spinner animation="border" variant="dark" />
-        )}
-      </div>
-      
-)
-}`}</code>
-
-<code className="language-javascript"> {`
-
-<=======ApiDataFetch.js=======>
-
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-
-
-const ApiDataTableFetch = (url)=> {
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] =  useState(null);
-
-    useEffect(() => {
-        setLoading(true);
-        axios.get(url)
-        .then((res)=>{
-            setData(res.data)
-        })
-        .catch((err)=>{
-            setError(err)
-        })
-        .finally(()=>{
-            setLoading(false);
-        })
-    },[url]);
-    return {data,loading, error}
+        <MaterialReactTable
+          columns={columns}
+          data={data}
+          enableColumnActions={true}
+          enableColumnFilters={false}
+          enablePagination={true}
+          enableSorting={true}
+          enableBottomToolbar={true}
+          enableTopToolbar={false}
+          muiTableBodyRowProps={{ hover: false }}
+          muiTablePaperProps={{ sx: { borderRadius: '8px' } }}
+          muiTableHeadRowProps={{ sx: { backgroundColor: '#01579b' } }}
+          muiTableHeadCellProps={{ sx: { color: "#fff" } }}
+        />
+      </div>`}</code><code className="language-javascript">{`
+  );
 }
-export {ApiDataTableFetch}`}
-        </code>
-</pre>
-</div>
+`}</code>
+        </pre>
+      </div>
     </>
   )
 }
